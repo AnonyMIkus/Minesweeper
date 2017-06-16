c1f907af8278978ec611a97deb45c2e3bb37ffcc
Sandra <test@143.93.88.53>
2017-06-16 16:07:03 +0200

    neighbourCells als Liste

--- Seng_Projekt/src/Cell.java
+++ Seng_Projekt/src/Cell.java
@@ -1,11 +1,11 @@
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Image;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -13,9 +13,8 @@ import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 import javax.swing.border.LineBorder;
-import java.awt.event.MouseAdapter;
 
-public class Cell implements ActionListener{
+public class Cell implements ActionListener {
 	private JButton button;
 	private JLabel label;
 	boolean bomb = false;
@@ -25,100 +24,91 @@ public class Cell implements ActionListener{
 	Game game;
 	ImageIcon ico;
 	boolean uncovered = false;
-	Cell[] neighbourCells;
+	ArrayList<Cell> neighbourCells;
 	public int dimensionX;
 	public int dimensionY;
-	
-	
-	public Cell(Game game, int[] index, ImageIcon flagItem){
-		
-        button = new JButton();
-        this.ico = flagItem;
-       
-        
-        label = new JLabel();
-        label.setVerticalAlignment(SwingConstants.CENTER);
-        label.setHorizontalAlignment(SwingConstants.CENTER);
-        label.setPreferredSize(new Dimension(20,20));
-        label.setBorder(new LineBorder(Color.BLACK, 1));
-        button.addActionListener(this);
-        button.setPreferredSize(new Dimension(20,20));
-        button.setMargin(new Insets(0,0,0,0));
-        this.index = index;
-        this.game = game;
-        this.dimensionX = game.dimensionX;
-        this.dimensionY = game.dimensionY;
-        
-        button.addMouseListener(new MouseAdapter(){
-      	  public void mouseReleased(MouseEvent e){
-      		if (SwingUtilities.isRightMouseButton(e)){
-      			
-	      	    if(!flagSet){
-	      	    	button.setIcon(ico);
-	      	    	flagSet = true;
-	      	    }
-	      	    else{
-	      	    	button.setIcon(null);
-	      	    	flagSet = false;
-	      	    }
-	      	    }
-      	  }
-      	  
-      });
-    
-  
-    }
+
+	public Cell(Game game, int[] index, ImageIcon flagItem) {
+
+		button = new JButton();
+		this.ico = flagItem;
+
+		label = new JLabel();
+		label.setVerticalAlignment(SwingConstants.CENTER);
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		label.setPreferredSize(new Dimension(20, 20));
+		label.setBorder(new LineBorder(Color.BLACK, 1));
+		button.addActionListener(this);
+		button.setPreferredSize(new Dimension(20, 20));
+		button.setMargin(new Insets(0, 0, 0, 0));
+		this.index = index;
+		this.game = game;
+		this.dimensionX = game.dimensionX;
+		this.dimensionY = game.dimensionY;
+
+		button.addMouseListener(new MouseAdapter() {
+			public void mouseReleased(MouseEvent e) {
+				if (SwingUtilities.isRightMouseButton(e)) {
+
+					if (!flagSet) {
+						button.setIcon(ico);
+						flagSet = true;
+					} else {
+						button.setIcon(null);
+						flagSet = false;
+					}
+				}
+			}
+
+		});
+
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		
+
 		uncover();
-		
-		if(bomb){
+
+		if (bomb) {
 			GameOver gameOver = new GameOver(new Minesweeper());
 			gameOver.setVisible(true);
 			game.frmMinesweeper.dispose();
 		}
-		//if(this.getValue() == 0 && !this.bomb)
-			//uncoverNeighbours();
-		game.uncover(index);
-		
+		if(this.getValue() == 0 && !this.bomb)
+		uncoverNeighbours();
+		//game.uncover(index);
+
 	}
-	
-	public void uncover(){
-		if(!flagSet){
+
+	public void uncover() {
+		if (!flagSet) {
 			getButton().setVisible(false);
 			uncovered = true;
 		}
 	}
-	
-	public void uncoverNeighbours(){
-		
+
+	public void uncoverNeighbours() {
+
 		getNeighbours();
 
-		
-		for(int i = 0; i < neighbourCells.length; i++){
-			if(neighbourCells[i] != null){
-				if(!neighbourCells[i].uncovered && (!neighbourCells[i].bomb || neighbourCells[i].flagSet))
-					neighbourCells[i].uncover();
-				if(neighbourCells[i].getValue() == 0)
-					neighbourCells[i].uncoverNeighbours();
-			}
+		for (int i = 0; i < neighbourCells.size(); i++) {
+			if (!neighbourCells.get(i).uncovered && (!neighbourCells.get(i).bomb || neighbourCells.get(i).flagSet))
+				neighbourCells.get(i).uncover();
+			if (neighbourCells.get(i).getValue() == 0)
+				neighbourCells.get(i).uncoverNeighbours();
 		}
-		
+
 	}
-	
-	
-	
-	public void setBomb(){
+
+	public void setBomb() {
 		bomb = true;
 		label.setText("X");
 		label.setForeground(Color.RED);
 	}
-	
-	 public JButton getButton() {
-	        return button;
-	    }
+
+	public JButton getButton() {
+		return button;
+	}
 
 	public JLabel getLabel() {
 		return label;
@@ -127,51 +117,45 @@ public class Cell implements ActionListener{
 	public void setLabel(JLabel label) {
 		this.label = label;
 	}
-	
-	public void setValue(int val){
-		if(!bomb){
+
+	public void setValue(int val) {
+		if (!bomb) {
 			value = val;
 			label.setText(Integer.toString(value));
-			}
+		}
 	}
-	
-	public int getValue(){
+
+	public int getValue() {
 		return value;
 	}
-	
-	public int[] getIndex(){
+
+	public int[] getIndex() {
 		return this.index;
 	}
-	
-	private void getNeighbours(){
-		
+
+	private void getNeighbours() {
+
 		int x = index[0];
 		int y = index[1];
-		
-		neighbourCells = new Cell[] {null, null, null, null, null, null, null, null};
-		
-		if(y != dimensionY-1)
-			neighbourCells[0] = game.getCellbyIndex(new int[]{x, y+1});
-		if(y != 0)
-			neighbourCells[1] = game.getCellbyIndex(new int[]{x, y-1});
-		if(x != dimensionX-1)
-			neighbourCells[2] = game.getCellbyIndex(new int[]{x+1, y});
-		if(x != 0)
-			neighbourCells[3] = game.getCellbyIndex(new int[]{x-1, y});
-		if(x != dimensionX-1 && y != dimensionY-1)
-			neighbourCells[4] = game.getCellbyIndex(new int[]{x+1, y+1});
-		if(x != dimensionX-1 && y != 0)
-			neighbourCells[5] = game.getCellbyIndex(new int[]{x+1, y-1});
-		if(x != 0 && y != dimensionY-1)
-			neighbourCells[6] = game.getCellbyIndex(new int[]{x-1, y+1});
-		if(x != 0 && y != 0)
-			neighbourCells[7] = game.getCellbyIndex(new int[]{x-1, y-1});
+
+		neighbourCells = new ArrayList<Cell>();
+
+		if (y != dimensionY - 1)
+			neighbourCells.add(game.getCellbyIndex(new int[] { x, y + 1 }));
+		if (y != 0)
+			neighbourCells.add(game.getCellbyIndex(new int[] { x, y - 1 }));
+		if (x != dimensionX - 1)
+			neighbourCells.add(game.getCellbyIndex(new int[] { x + 1, y }));
+		if (x != 0)
+			neighbourCells.add(game.getCellbyIndex(new int[] { x - 1, y }));
+		if (x != dimensionX - 1 && y != dimensionY - 1)
+			neighbourCells.add(game.getCellbyIndex(new int[] { x + 1, y + 1 }));
+		if (x != dimensionX - 1 && y != 0)
+			neighbourCells.add(game.getCellbyIndex(new int[] { x + 1, y - 1 }));
+		if (x != 0 && y != dimensionY - 1)
+			neighbourCells.add(game.getCellbyIndex(new int[] { x - 1, y + 1 }));
+		if (x != 0 && y != 0)
+			neighbourCells.add(game.getCellbyIndex(new int[] { x - 1, y - 1 }));
 	}
-	
-	
 
-	
-	
 }
-
-
--- Seng_Projekt/src/Minesweeper.java
+++ Seng_Projekt/src/Minesweeper.java
@@ -31,7 +31,6 @@ public class Minesweeper {
 	boolean selectedErf = false;
 	boolean selectedProf = false;
 	Minesweeper menu = this;
-	
 
 	/**
 	 * Launch the application.
@@ -60,117 +59,114 @@ public class Minesweeper {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		
-		
+
 		frmMinesweeper = new JFrame();
 		frmMinesweeper.setTitle("Minesweeper");
 		frmMinesweeper.setBounds(500, 200, 451, 392);
 		frmMinesweeper.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmMinesweeper.getContentPane().setLayout(null);
-		
+
 		initializeButtons();
-		
-		
+
 		JTextArea txtrSoftwareengineering = new JTextArea();
 		txtrSoftwareengineering.setEditable(false);
 		txtrSoftwareengineering.setText("Software-Engineering \nProjekt von \nSandra, Philipp, John, Chris");
 		txtrSoftwareengineering.setBounds(36, 267, 185, 55);
 		frmMinesweeper.getContentPane().add(txtrSoftwareengineering);
-		
+
 		JLabel lblMinesweeper = new JLabel("Minesweeper");
 		lblMinesweeper.setFont(new Font("Arial Black", Font.BOLD, 22));
 		lblMinesweeper.setBounds(126, 30, 196, 23);
 		frmMinesweeper.getContentPane().add(lblMinesweeper);
-		
+
 		JMenuBar bar = new JMenuBar();
 		JMenu file = new JMenu("File");
 		JMenuItem close = new JMenuItem("Close");
-		
+
 		file.add(close);
-		
+
 		close.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				System.exit(0);
 			}
-});
-		
+		});
+
 		bar.add(file);
-		
+
 		frmMinesweeper.setJMenuBar(bar);
-		
-		
-		
-		
+
 	}
 
 	private void initializeButtons() {
-		
+
 		ButtonGroup bGroupGame = new ButtonGroup();
 		ButtonGroup bGroupDiff = new ButtonGroup();
-		
+
 		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Spiel", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Spiel", TitledBorder.LEADING,
+				TitledBorder.TOP, null, new Color(0, 0, 0)));
 		panel.setBounds(82, 85, 118, 166);
 		frmMinesweeper.getContentPane().add(panel);
 		panel.setLayout(null);
-		
+
 		JToggleButton btnStandard = new JToggleButton("Standard");
 		btnStandard.setSelected(true);
 		btnStandard.setBounds(6, 16, 106, 46);
 		panel.add(btnStandard);
-		
+
 		JToggleButton btnTreasure = new JToggleButton("Treasure Hunt");
 		btnTreasure.setBounds(6, 64, 106, 46);
 		panel.add(btnTreasure);
-		
+
 		JToggleButton btnBomb = new JToggleButton("Bomb Game");
 		btnBomb.setBounds(6, 113, 106, 46);
 		panel.add(btnBomb);
-		
+
 		bGroupGame.add(btnStandard);
 		bGroupGame.add(btnTreasure);
 		bGroupGame.add(btnBomb);
-		
+
 		JPanel panel_1 = new JPanel();
-		panel_1.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Schwierigkeitsgrad", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel_1.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Schwierigkeitsgrad",
+				TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
 		panel_1.setBounds(247, 83, 118, 166);
 		frmMinesweeper.getContentPane().add(panel_1);
 		panel_1.setLayout(null);
-		
+
 		JToggleButton btnEasy = new JToggleButton("Anf\u00E4nger");
 		btnEasy.setSelected(true);
 		btnEasy.setBounds(6, 16, 106, 46);
 		panel_1.add(btnEasy);
-		
+
 		JToggleButton btnMedium = new JToggleButton("Erfahren");
 		btnMedium.setBounds(6, 64, 106, 46);
 		panel_1.add(btnMedium);
-		
+
 		JToggleButton btnPro = new JToggleButton("Profi");
 		btnPro.setBounds(6, 113, 106, 46);
 		panel_1.add(btnPro);
-		
+
 		bGroupDiff.add(btnEasy);
 		bGroupDiff.add(btnMedium);
 		bGroupDiff.add(btnPro);
-		
+
 		JButton btnStart = new JButton("Start");
 		btnStart.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				frmMinesweeper.dispose();
 				Game game = new Game(btnEasy.isSelected() ? 0 : btnMedium.isSelected() ? 1 : 2, menu);
-			
+
 			}
 		});
 
 		btnStart.setBounds(276, 279, 89, 23);
 		frmMinesweeper.getContentPane().add(btnStart);
-		
+
 	}
-	
-	public JFrame getFrame(){
+
+	public JFrame getFrame() {
 		return this.frmMinesweeper;
 	}
 }
