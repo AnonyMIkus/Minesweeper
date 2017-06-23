d27f41f5664d22de629f0c6b2fbf691f90bafff1
Admin <Admin@192.168.44.102>
2017-06-23 15:55:36 +0200

    Library of Elements

---
+++ Seng_Projekt/src/gameLibrary.java
@@ -0,0 +1,112 @@
+
+public class gameLibrary {
+	private int mode;
+	
+	private int panelAnfPosX = 49;
+	private int panelAnfPosY = 72;
+	private int panelFortPosX = 49;
+	private int panelFortPosY = 72;
+	private int panelProPosX = 49;
+	private int panelProPosY = 72;
+	private int panelAnfWidth = 388;
+	private int panelFortWidth = 488;
+	private int panelProWidth = 850;
+	private int dimensionX;
+	private int dimensionY;
+
+	public int getMode() {
+		return mode;
+	}
+
+	public void setMode(int mode) {
+		this.mode = mode;
+	}
+
+	public int getPanelAnfPosX() {
+		return panelAnfPosX;
+	}
+
+	public void setPanelAnfPosX(int panelAnfPosX) {
+		this.panelAnfPosX = panelAnfPosX;
+	}
+
+	public int getPanelAnfPosY() {
+		return panelAnfPosY;
+	}
+
+	public void setPanelAnfPosY(int panelAnfPosY) {
+		this.panelAnfPosY = panelAnfPosY;
+	}
+
+	public int getPanelFortPosX() {
+		return panelFortPosX;
+	}
+
+	public void setPanelFortPosX(int panelFortPosX) {
+		this.panelFortPosX = panelFortPosX;
+	}
+
+	public int getPanelFortPosY() {
+		return panelFortPosY;
+	}
+
+	public void setPanelFortPosY(int panelFortPosY) {
+		this.panelFortPosY = panelFortPosY;
+	}
+
+	public int getPanelProPosX() {
+		return panelProPosX;
+	}
+
+	public void setPanelProPosX(int panelProPosX) {
+		this.panelProPosX = panelProPosX;
+	}
+
+	public int getPanelProPosY() {
+		return panelProPosY;
+	}
+
+	public void setPanelProPosY(int panelProPosY) {
+		this.panelProPosY = panelProPosY;
+	}
+
+	public int getPanelAnfWidth() {
+		return panelAnfWidth;
+	}
+
+	public void setPanelAnfWidth(int panelAnfWidth) {
+		this.panelAnfWidth = panelAnfWidth;
+	}
+
+	public int getPanelFortWidth() {
+		return panelFortWidth;
+	}
+
+	public void setPanelFortWidth(int panelFortWidth) {
+		this.panelFortWidth = panelFortWidth;
+	}
+
+	public int getPanelProWidth() {
+		return panelProWidth;
+	}
+
+	public void setPanelProWidth(int panelProWidth) {
+		this.panelProWidth = panelProWidth;
+	}
+
+	public int getDimensionX() {
+		return dimensionX;
+	}
+
+	public void setDimensionX(int dimensionX) {
+		this.dimensionX = dimensionX;
+	}
+
+	public int getDimensionY() {
+		return dimensionY;
+	}
+
+	public void setDimensionY(int dimensionY) {
+		this.dimensionY = dimensionY;
+	}
+}
