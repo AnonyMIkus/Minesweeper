8080d1130913126419a4a97c79021f08c396496c
AnonyMIkus <AnonyMIkus@192.168.44.102>
2017-07-03 01:54:47 +0200

    Correctly working + Comments.

--- Seng_Projekt/src/HighscoreFrame.java
+++ Seng_Projekt/src/HighscoreFrame.java
@@ -1,6 +1,12 @@
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
@@ -8,65 +14,206 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 import javax.swing.border.Border;
 
+/**
+ * HighscoreFrame is loading the Highscore for the gamemodes and their levels
+ * beginner, amateur and advanced.
+ * 
+ * @author John Voronkov
+ *
+ * @version 2.0
+ */
+
 public class HighscoreFrame {
+
+	private static String[] fileName = { "beginnerstandard.txt", "amateurstandard.txt", "advancedstandard.txt",
+			"beginnertreasure.txt", "amateurtreasure.txt", "advancedtreasure.txt", "beginnerbomb.txt",
+			"amateurbomb.txt", "advancedbomb.txt" };
+	private static String[] Title = { "Highscore Beginner Standard", "Highscore Amateur Standard",
+			"Highscore Advanced Standard", "Highscore Beginner Treasure Hunt", "Highscore Amateur Treasure Hunt",
+			"Highscore Advanced Treasure Hunt", "Highscore Beginner Bomb Hunt", "Highscore Amateur Bomb Hunt",
+			"Highscore Advanced Bomb Hunt" };
 	private final int topNumber = 25;
-
-	// Listen
-	private ArrayList<String> playerName = new ArrayList<>();
-	private ArrayList<Integer> playedTime = new ArrayList<>();
-
+	gameLibrary gl;
+	private String seperator = ",";
+	private ArrayList<String> playerList = new ArrayList<>();
+	private ArrayList<Integer> timeList = new ArrayList<>();
 	private ArrayList<JLabel> rangText = new ArrayList<>();
 	private ArrayList<JLabel> playerText = new ArrayList<>();
 	private ArrayList<JLabel> timeText = new ArrayList<>();
-
-	// Intinitalisierung des Fensters.
 	private JFrame highscoreFrame = new JFrame("Highscore");
-	private JPanel listPanel = new JPanel(new GridLayout(1, 3));
+	private JPanel listPanel = new JPanel(new GridLayout(1, 4));
 	private JPanel postionPanel = new JPanel(new GridLayout(topNumber, 1));
 	private JPanel playerPanel = new JPanel(new GridLayout(topNumber, 1));
 	private JPanel timerPanel = new JPanel(new GridLayout(topNumber, 1));
 	private JPanel framePanel = new JPanel();
+	private int usedTime = Game.getGl().getTime();
+	private String playerName = Game.getGl().getPlayerName();
+	private int mode = Game.getGl().getMode();
 
-	// Ininitalisierung der Elemente.
-	private JTextField inputName = new JTextField("Player 1");
-	private int used = 935; // Hier kommt die gespielte Zeit hin. Vorerste Werte
-							// zum Testen.
-	private JLabel receivedTime = new JLabel(String.valueOf(used));
-	private JLabel sec = new JLabel("sec");
-	private JButton submitter = new JButton("Submit");
+	/**
+	 * Default Constructor for the process after a winning game.
+	 */
+	public HighscoreFrame() {
+		gl = Game.getGl();
+		load();
+		addScore();
+		save();
+		loadingList();
+		createFrame();
+	}
 
-	public void creatingHighscoreFrame() {
-		for (int i = 0; i < topNumber; i++) {
-			rangText.add(new JLabel(String.valueOf(i + 1)));
-			if (i < playerName.size()) {
-				playerText.add(new JLabel(playerName.get(i)));
-			} else {
-				playerText.add(new JLabel("No Player found"));
+	/**
+	 * Overloaded Constructor for calling Highscore from main menu.
+	 * 
+	 * @param mode
+	 *            is important to load the right document for score.
+	 */
+	public HighscoreFrame(int mode) {
+		highscoreFrame.setTitle(HighscoreFrame.Title[mode]);
+		load(mode);
+		loadingList();
+		createFrame();
+	}
+
+	/**
+	 * Saving Highscore into right document.
+	 * 
+	 * @return boolen for success of saving.
+	 */
+	public boolean save() {
+		try {
+			if (mode < 0 || mode > 8) {
+				return false;
 			}
-			if (i < playedTime.size()) {
-				timeText.add(new JLabel(String.valueOf(playedTime.get(i)) + " sec"));
-			} else {
-				timeText.add(new JLabel("999 sec"));
+			File saving = new File(fileName[mode]);
+			FileWriter fw = new FileWriter(saving);
+			BufferedWriter bw = new BufferedWriter(fw);
+			for (int i = 0; i < playerList.size(); i++) {
+				bw.write(playerList.get(i));
+				bw.write(seperator);
+				bw.write(String.valueOf(timeList.get(i)));
+				bw.newLine();
+			}
+			bw.close();
+			fw.close();
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	/**
+	 * Adding Score to Highscore if player did a Highscore in last game.
+	 */
+	public void addScore() {
+		int i = 0;
+		if (timeList.size() == 0) {
+			playerList.add(playerName);
+			timeList.add(usedTime);
+		} else {
+			while (usedTime > timeList.get(i) && i < topNumber) {
+				i++;
+			}
+			if (usedTime < timeList.get(i) && i < topNumber) {
+				playerList.add(playerName);
+				timeList.add(usedTime);
+			}
+			while (timeList.size() > topNumber) {
+				timeList.remove(topNumber);
+				timeList.trimToSize();
+				playerList.remove(topNumber);
+				playerList.trimToSize();
 			}
 		}
+	}
+
+	/**
+	 * Loading nessecary files depending on gamemode and difficulty.
+	 * 
+	 * @return boolen for success of loading.
+	 */
+	public boolean load() {
+		try {
+			if (mode < 0 || mode > 8) {
+				return false;
+			}
+			File saving = new File(fileName[mode]);
+			if (saving.exists()) {
+				FileReader fr = new FileReader(saving);
+				BufferedReader br = new BufferedReader(fr);
+				String bufferedReader = null;
+				if (br.ready()) {
+					bufferedReader = br.readLine();
+				}
+				String[] valueList;
+				while (!bufferedReader.equals(null)) {
+					valueList = bufferedReader.split(seperator);
+					playerList.add(valueList[0]);
+					timeList.add(Integer.parseInt(valueList[1]));
+					if (br.ready()) {
+						bufferedReader = br.readLine();
+					} else {
+						break;
+					}
+				}
+				br.close();
+				fr.close();
+				return true;
+			} else {
+				if (saving.createNewFile()) {
+					return true;
+				}
+				return false;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	/**
+	 * Preparing List for window.
+	 */
+	public void loadingList() {
+		for (int i = 0; i < topNumber; i++) {
+			rangText.add(new JLabel(String.valueOf(i + 1)));
+			if (i < playerList.size() && i < topNumber) {
+				playerText.add(new JLabel(playerList.get(i)));
+			} else {
+				playerText.add(new JLabel("No player found"));
+			}
+			if (i < timeList.size() && i < topNumber) {
+				timeText.add(new JLabel(String.valueOf(timeList.get(i))));
+			} else {
+				timeText.add(new JLabel("No time found"));
+			}
+		}
+
 		for (int i = 0; i < topNumber; i++) {
 			postionPanel.add(rangText.get(i));
 			playerPanel.add(playerText.get(i));
 			timerPanel.add(timeText.get(i));
 		}
+	}
 
-		submitter.addActionListener(new ActionListener() {
+	/**
+	 * Creating Window with given information.
+	 */
+	public void createFrame() {
+		JButton backButton = new JButton("Back to Menu");
+		backButton.setBounds(510, 20, 150, 30);
+		backButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (check()) {
-					refresh();
-				}
+				highscoreFrame.dispose();
+				highscoreFrame.setVisible(false);
+				Minesweeper ms = new Minesweeper();
+				ms.getFrame().setVisible(true);
 			}
 		});
 
-		// Aussehen der Listen definieren und hinzufügen.
 		Border rangBorder = BorderFactory.createTitledBorder("Rang");
 		Border playerBorder = BorderFactory.createTitledBorder("Player Name");
 		Border timeBorder = BorderFactory.createTitledBorder("Time");
@@ -79,94 +226,60 @@ public class HighscoreFrame {
 		listPanel.add(playerPanel);
 		listPanel.add(timerPanel);
 
-		// Elemente dem Fenster hinzufügen.
 		framePanel.add(listPanel);
-		framePanel.add(inputName);
-		framePanel.add(receivedTime);
-		framePanel.add(sec);
-		framePanel.add(submitter);
 
+		highscoreFrame.add(backButton);
+		highscoreFrame.setSize(700, 500);
 		highscoreFrame.add(framePanel);
 		highscoreFrame.setVisible(true);
 		highscoreFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		highscoreFrame.pack();
+		highscoreFrame.setTitle(Title[Game.getGl().getMode()]);
 	}
 
-	public boolean check() {
-		boolean checkScore = false;
-		int i = 0;
-		while (i <= playedTime.size() && i < topNumber) {
-			if (playedTime.size() != 0) {
-				if (used <= playedTime.get(i)) {
-					playedTime.add(i, used);
-					playerName.add(i, inputName.getText());
-					if (playedTime.size() >= topNumber) {
-						while (playedTime.size() > topNumber) {
-							playedTime.remove(topNumber);
-							playerName.remove(topNumber);
-						}
-					}
-					return checkScore;
+	/**
+	 * Loading nessecary files depending on gamemode and difficulty.
+	 * 
+	 * @param mode
+	 *            for loading correct file.
+	 * @return boolen for success of loading.
+	 */
+
+	public boolean load(int mode) {
+		try {
+			if (mode < 0 || mode > 8) {
+				return false;
+			}
+			File saving = new File(fileName[mode]);
+			if (saving.exists()) {
+				FileReader fr = new FileReader(saving);
+				BufferedReader br = new BufferedReader(fr);
+				String bufferedReader = null;
+				if (br.ready()) {
+					bufferedReader = br.readLine();
 				}
+				String[] valueList;
+				while (!bufferedReader.equals(null)) {
+					valueList = bufferedReader.split(seperator);
+					playerList.add(valueList[0]);
+					timeList.add(Integer.parseInt(valueList[1]));
+					if (br.ready()) {
+						bufferedReader = br.readLine();
+					} else {
+						break;
+					}
+				}
+				br.close();
+				fr.close();
+				return true;
 			} else {
-				playedTime.add(i, used);
-				playerName.add(i, inputName.getText());
-				return checkScore;
+				if (saving.createNewFile()) {
+					return true;
+				}
+				return false;
 			}
-			i++;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
 		}
-		return checkScore;
-	}
-
-	public void refresh() {
-		clearing();
-		addScores();
-	}
-
-	public void addScores() {
-		for (int i = 0; i < topNumber; i++) {
-			rangText.add(new JLabel(String.valueOf(i + 1)));
-			if (i < playerName.size()) {
-				playerText.add(new JLabel(playerName.get(i)));
-			} else {
-				playerText.add(new JLabel("No Player found"));
-			}
-			if (i < playedTime.size()) {
-				timeText.add(new JLabel(String.valueOf(playedTime.get(i)) + " sec"));
-			} else {
-				timeText.add(new JLabel("999 sec"));
-			}
-		}
-		for (int i = 0; i < topNumber; i++) {
-			postionPanel.add(rangText.get(i));
-			playerPanel.add(playerText.get(i));
-			timerPanel.add(timeText.get(i));
-		}
-	}
-
-	public void clearing() {
-		postionPanel.removeAll();
-		playerPanel.removeAll();
-		timerPanel.removeAll();
-		for (int i = 0; i < playerText.size();) {
-			rangText.remove(i);
-			rangText.trimToSize();
-		}
-		for (int i = 0; i < playerText.size();) {
-			playerText.remove(i);
-			playerText.trimToSize();
-		}
-		for (int i = 0; i < playerText.size();) {
-			timeText.remove(i);
-			timeText.trimToSize();
-		}
-	}
-
-	public void save() {
-
-	}
-
-	public void load() {
-
 	}
 }
